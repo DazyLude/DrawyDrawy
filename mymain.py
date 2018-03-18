28826420bf0676cc2c83c91adc2ec8720742944c
DazyLude <36658009+DazyLude@users.noreply.github.com>
2018-03-18 21:09:27 +0300

    Added control window, made widget self sufficient

--- mymain.py
+++ mymain.py
@@ -10,21 +10,9 @@ import sys
 from PyQt5 import QtWidgets
 import ayyy_draw as graph
 
-class YourFace(QtWidgets.QDialog):
-    def __init__(self,parent=None):
-        super().__init__(parent)
-        self.setGeometry(50,50,1280,720)
-        self.setWindowTitle("DrawyDrawy more like it's haaaaaa noon")
-        
-        self.DrawyDrawy=graph.MyFace(self)
-        self.DrawyDrawy.setGeometry(0,0,1280,720)
-    
-    def resizeEvent(self, event):
-        self.DrawyDrawy.setGeometry(0,0,self.width(),self.height())
-
 
 app = QtWidgets.QApplication(sys.argv)
-w = YourFace()
+w = graph.YourFace()
 x = [0,1,2,3,4,5,6,7,8,9.]
 y = [1,2,1,5,-0.00585,7.576,4,6,3,1.]
 w.show()
