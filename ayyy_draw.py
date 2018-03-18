6515180fdb2f3bb6bfcf15da66fe6e5395b8c4da
DazyLude <36658009+DazyLude@users.noreply.github.com>
2018-03-18 13:32:26 +0300

    pushing while it still works
    +ceil/floor functions that work with significant figures
    +rounding of the min/max values to the needed significant figure
    +drawing grids, code is ready to add writing of values next to them
    +a couple of variables that are encountered more than once

--- ayyy_draw.py
+++ ayyy_draw.py
@@ -10,6 +10,24 @@ Created on Sun Mar  4 14:56:07 2018
 from PyQt5 import QtGui, QtCore, QtWidgets
 import numpy as np
 
+#rounding abs to the needed significant figures up and down
+def rsfceil(x, sf = 2):
+    if x==0.:
+        return 0.
+    else:
+        sf = sf-1
+        deg = 10**(np.floor(np.log10(abs(x)))-sf)
+        return np.ceil(x/deg)*deg
+
+def rsffloor(x, sf = 2):
+    if x==0.:
+        return 0.
+    else:
+        sf = sf-1
+        deg = 10**(np.floor(np.log10(abs(x)))-sf)
+        return np.floor(x/deg)*deg
+
+
 class MyFace(QtWidgets.QWidget):
     def __init__(self,parent=None):
         super().__init__(parent)
@@ -17,7 +35,7 @@ class MyFace(QtWidgets.QWidget):
         self.Pbegin=1
         self.PointsOnScreen=10
         self.f=np.array([[0,1,2,3,4,5,6,7,8,9.],
-                        [1,2,1,5,-4,7,4,6,3,1.]])
+                        [1,2,1,5,-0.0585,7.576,4,6,3,1.]])
         self.gap=10
         self.BrushSize=2
     
@@ -26,6 +44,7 @@ class MyFace(QtWidgets.QWidget):
             self.f = np.array(x, y)
         else:
             print("length of x inequal to length of y!")
+        
         return
     
     def paintEvent(self,event):
@@ -36,11 +55,16 @@ class MyFace(QtWidgets.QWidget):
         BS=self.BrushSize
         h=self.height()
         w=self.width()
+        hdiv=4.     #amount of grids
+        wdiv=10.    #amount of grids
+        
         
         lgp = 3*gp  #left gap
         rgp = gp    #right
         tgp = gp    #top
         bgp = 2*gp  #bottom
+        drawh = h-tgp-bgp   #width of the drawing field
+        draww = w-lgp-rgp   #heigth of the drawing field
         
         qp=QtGui.QPainter()
         qp.begin(self)
@@ -58,10 +82,12 @@ class MyFace(QtWidgets.QWidget):
         #normalization
         if POS>len(self.f[0]):
             POS=len(self.f[0])
-        maxy = np.max(self.f[1][P0:P0+POS])
-        miny = np.min(self.f[1][P0:P0+POS])
-        maxx = np.max(self.f[0][P0:P0+POS])
-        minx = np.min(self.f[0][P0:P0+POS])
+        maxy = rsfceil(np.max(self.f[1][P0:P0+POS]))
+        miny = rsffloor(np.min(self.f[1][P0:P0+POS]))
+        maxx = rsfceil(np.max(self.f[0][P0:P0+POS]))
+        minx = rsffloor(np.min(self.f[0][P0:P0+POS]))
+        #print(maxy, maxx, miny, minx)
+
         #anti division by 0 division
         if maxy == miny:
             miny = np.nan
@@ -76,18 +102,21 @@ class MyFace(QtWidgets.QWidget):
         fnormie = (self.f[1]-miny)/(maxy-miny)
         fnormx = (self.f[0]-minx)/(maxx-minx)
         
-        #Рисуем окно для графиков
+        #Drawing grids and graph border
         qp.setPen(pen1)
         qp.drawRect(self.rect().adjusted(lgp,tgp,-rgp,-bgp))
-#        #Рисуем полоски на 0, 0.5 вверх и вниз
-#        qp.setPen(pen2)
-#        for hei in (h/4, h/2, h*3/4):
-#            q=(QtCore.QPointF(gp,hei),QtCore.QPointF(w-gp,hei))
-#            qp.drawLine(q[0],q[1])
-#        #Рисуем полоски по вертикали, 9 шт
-#        for wid in [(w-gp*2)*k/10+gp for k in range(1,10)]:
-#            q=(QtCore.QPointF(wid,gp),QtCore.QPointF(wid,h-gp))
-#            qp.drawLine(q[0],q[1])
+        #horizontal grids
+        qp.setPen(pen2)
+        for parth in np.arange(1/hdiv, 1, 1/hdiv):
+            hei=drawh*parth+tgp
+            q=(QtCore.QPointF(lgp,hei),QtCore.QPointF(w-rgp,hei))
+            qp.drawLine(q[0],q[1])
+        
+        #vertical grips
+        for partw in np.arange(1/wdiv, 1, 1/wdiv):
+            wid=draww*partw+lgp
+            q=(QtCore.QPointF(wid,tgp),QtCore.QPointF(wid,h-bgp))
+            qp.drawLine(q[0],q[1])
         
         
         #Создаем массивы - графики
